68b4e4db3bc5264f5d4377b66838d807890917ce
dmychel <dmychel@tutanota.com>
2024-10-13 17:57:37 -0400

    CREATED HEADER COMP

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <a href="">HOME</a>
+        <a href="">ABOUT</a>
+      </nav>
+
+      <nav>
+        <a href="">SHOP</a>
+        <a href="">CONTACT</a>
+      </nav>
+    </header>
+  );
+}
