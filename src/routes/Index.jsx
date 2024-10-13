6f15483253a2cb49b8ac6e39358c9e1879fdf3bc
dmychel <dmychel@tutanota.com>
2024-10-13 18:54:53 -0400

    created array and logic to render jsx cards

--- src/routes/Index.jsx
+++ src/routes/Index.jsx
@@ -1,3 +1,40 @@
+import styles from "/styles/home.module.scss";
+
 export default function Index() {
-  return <h1>Index</h1>;
+  const arr = [
+    {
+      title: "DIGITAL ART",
+      cover: "https://placehold.co/600x400",
+      link: "",
+    },
+    {
+      title: "Photography",
+      cover: "https://placehold.co/600x400",
+      link: "",
+    },
+    {
+      title: "COMMISSIONS",
+      cover: "https://placehold.co/600x400",
+      link: "",
+    },
+    {
+      title: "DESIGN",
+      cover: "https://placehold.co/600x400",
+      link: "",
+    },
+  ];
+
+  return (
+    <div className={styles.home}>
+      {arr.map((card, index) => (
+        <div
+          className={styles.card}
+          key={index}
+          style={{ backgroundImage: `url("https://placehold.co/600x400")` }}
+        >
+          <h2>{card.title}</h2>
+        </div>
+      ))}
+    </div>
+  );
 }
