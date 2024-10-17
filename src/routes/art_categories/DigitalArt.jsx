1920587de667dbeb93e6910845538ce712731a9c
dmychel <dmychel@tutanota.com>
2024-10-16 23:42:16 -0400

    Created logic for rendering links

--- src/routes/art_categories/DigitalArt.jsx
+++ src/routes/art_categories/DigitalArt.jsx
@@ -1,3 +1,22 @@
+import styles from '/styles/artDesign.module.scss'
+
 export default function DigitalArt() {
-  return <h1>Digital Art</h1>;
+  const arr = [
+    "whitney-desktop.jpg",
+    "alejandro.png",
+    "mac.png",
+    "marvin.jpg",
+    "neo.png",
+    "biggie.jpg",
+    "TORQUE24.png",
+  ]
+  return (
+    <div className={styles.gallery}>
+      <div>
+        {arr.map((picture, index) => (
+          <img key={index} src={`https://dmychel.github.io/dabbler-media/art_design/${picture}`} alt="" />
+        ))}
+      </div>
+    </div>
+  );
 }
