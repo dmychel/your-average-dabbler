1c4b11048f4cc9c81e109108e4af3cce8250b077
dmychel <dmychel@tutanota.com>
2024-10-17 18:37:17 -0400

    built logic to render photos

--- src/routes/art_categories/Photography.jsx
+++ src/routes/art_categories/Photography.jsx
@@ -1,3 +1,78 @@
+import Gallery from "../../components/Gallery";
+
 export default function Photography() {
-  return <h1>Photography</h1>;
+  const arr = [
+    {
+      picture: "DSC09353.jpg",
+      link: "https://dmychel.github.io/dabbler-media/photography",
+    },
+    {
+      picture: "198748293.jpg",
+      link: "https://dmychel.github.io/dabbler-media/photography",
+    },
+    {
+      picture: "201464821.jpg",
+      link: "https://dmychel.github.io/dabbler-media/photography",
+    },
+    {
+      picture: "209158259.jpgg",
+      link: "https://dmychel.github.io/dabbler-media/photography",
+    },
+    {
+      picture: "213040810.jpg",
+      link: "https://dmychel.github.io/dabbler-media/photography",
+    },
+    {
+      picture: "DSC01541.jpg",
+      link: "https://dmychel.github.io/dabbler-media/photography",
+    },
+    {
+      picture: "DSC02155.jpg",
+      link: "https://dmychel.github.io/dabbler-media/photography",
+    },
+    {
+      picture: "DSC02648_1.jpg",
+      link: "https://dmychel.github.io/dabbler-media/photography",
+    },
+    {
+      picture: "DSC06703.jpg",
+      link: "https://dmychel.github.io/dabbler-media/photography",
+    },
+    {
+      picture: "DSC08883.jpg",
+      link: "https://dmychel.github.io/dabbler-media/photography",
+    },
+    {
+      picture: "DSC09064.jpg",
+      link: "https://dmychel.github.io/dabbler-media/photography",
+    },
+    {
+      picture: "DSC09454.jpg",
+      link: "https://dmychel.github.io/dabbler-media/photography",
+    },
+    {
+      picture: "DSC09637.jpg",
+      link: "https://dmychel.github.io/dabbler-media/photography",
+    },
+    {
+      picture: "DSC09678.jpg",
+      link: "https://dmychel.github.io/dabbler-media/photography",
+    },
+    {
+      picture: "DSC09688.jpg",
+      link: "https://dmychel.github.io/dabbler-media/photography",
+    },
+    {
+      picture: "DSC09728.jpg",
+      link: "https://dmychel.github.io/dabbler-media/photography",
+    },
+  ];
+
+  return (
+    <div>
+      <div>
+        <Gallery arr={arr} />
+      </div>
+    </div>
+  );
 }
